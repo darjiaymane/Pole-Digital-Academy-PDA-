6ea1f0256f7f6b98bbc464d90a7f1165518febc6
armanovxx <iebeat2018@outlook.com>
2022-11-15 08:44:06 +0100

    login completed soon

--- src/main/java/com/example/poledigitalacademypda/Dao/Specs/UserDao.java
+++ src/main/java/com/example/poledigitalacademypda/Dao/Specs/UserDao.java
@@ -2,7 +2,6 @@ package com.example.poledigitalacademypda.Dao.Specs;
 
 import com.example.poledigitalacademypda.Dao.Dao;
 import com.example.poledigitalacademypda.Entities.User;
-import org.hibernate.SessionFactory;
 
 public interface UserDao extends Dao<User> {
 
